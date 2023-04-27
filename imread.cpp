41b396641ab758a08ea7026802e4bc9f095dc36d
Osamu Watanabe <owatanab@es.takushoku-u.ac.jp>
2023-04-27 10:37:05 +0900

    Change main() to get arguments from command-line

--- imread.cpp
+++ imread.cpp
@@ -1,9 +1,13 @@
 #include <cstdio>
 #include <opencv2/opencv.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("An input image file is missing.\n");
+    return EXIT_FAILURE;
+  }
   cv::Mat image;
-  image = cv::imread("barbara.pgm");
+  image = cv::imread(argv[1]);
   if (image.empty()) {
     printf("Image file is not found.\n");
     return EXIT_FAILURE;
